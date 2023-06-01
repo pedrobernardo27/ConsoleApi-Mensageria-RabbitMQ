f103a99ad1d3a1882c2ba89e996324e981b482b7
pedrobernardo27 <Josepedrobernardo@hotmail.com>
2023-06-01 11:07:55 -0300

    Add project files.

---
+++ Mensageria RabbitMQ/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Mensageria_RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {                
+                new Publicador().Publicar();
+
+                new Receptor().Consumir();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Mensageria RabbitMQ/Publicador.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Mensageria_RabbitMQ
+{
+    public class Publicador
+    {
+        public void Publicar()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = "localhost"
+                };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "teste",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                    const string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: string.Empty,
+                                         routingKey: "teste",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception($"{ex.Message}");
+            }
+        }
+    }
+}
